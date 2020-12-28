0a8646ca215f3a3da58b447ffb2a9adf298027a1
Hariokas <73305957+Hariokas@users.noreply.github.com>
2020-12-28 12:19:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int winNumber = r.Next(0, 100);
+            bool hasGuessedCorrectly = false;
+
+            while (hasGuessedCorrectly == false)
+            {
+                try
+                {
+                    Console.Write("Guess the number between 0 and 100!: ");
+                    var guessNumber = Int32.Parse(Console.ReadLine());
+
+                    if (guessNumber > winNumber)
+                        Console.WriteLine($"Try lower number!\n");
+
+                    if (guessNumber < winNumber)
+                        Console.WriteLine($"Try higher number!\n");
+
+                    if (guessNumber == winNumber)
+                    {
+                        hasGuessedCorrectly = true;
+                        Console.WriteLine($"You've guessed correctly! The secret number was {winNumber}\n");
+                    }
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Unexpected input!");
+                }
+            }
+            Console.WriteLine("Thank you for playing!\nPress any key to exit the game.");
+            Console.ReadKey(true);
+        }
+    }
+}
